e75ed5ea0a816bb34195db9ffa600345ebfa359e
skver <nulla.animo@gmail.com>
2016-09-13 09:47:37 +0300

    Init commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://mysite.zz/admin/');
+define('HTTP_CATALOG', 'http://mysite.zz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://mysite.zz/admin/');
+define('HTTPS_CATALOG', 'http://mysite.zz/');
+
+// DIR
+define('DIR_APPLICATION', '/home/karma/server/mysite.zz/admin/');
+define('DIR_SYSTEM', '/home/karma/server/mysite.zz/system/');
+define('DIR_LANGUAGE', '/home/karma/server/mysite.zz/admin/language/');
+define('DIR_TEMPLATE', '/home/karma/server/mysite.zz/admin/view/template/');
+define('DIR_CONFIG', '/home/karma/server/mysite.zz/system/config/');
+define('DIR_IMAGE', '/home/karma/server/mysite.zz/image/');
+define('DIR_CACHE', '/home/karma/server/mysite.zz/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/karma/server/mysite.zz/system/storage/download/');
+define('DIR_LOGS', '/home/karma/server/mysite.zz/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/karma/server/mysite.zz/system/storage/modification/');
+define('DIR_UPLOAD', '/home/karma/server/mysite.zz/system/storage/upload/');
+define('DIR_CATALOG', '/home/karma/server/mysite.zz/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'fenixes13');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
